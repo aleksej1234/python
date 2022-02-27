1da8cfdfeb693705bab923ec770bfa0e1d70c111
aleksej1234 <100515748+aleksej1234@users.noreply.github.com>
2022-02-27 18:25:17 +0700

    Create if.py

---
+++ if.py
@@ -0,0 +1,21 @@
+num=int(input("введи число"))
+if num == 0:
+    print("0")
+elif num==1:
+    print("1") 
+elif num ==2:
+    print("2")
+elif num==2:
+    print("2")
+elif num ==3:
+    print("3")
+elif num==3:
+    print("3")
+elif num ==4:
+    print("4")
+elif num==4:
+    print("4")
+elif num ==5:
+    print("5")
+elif num==5:
+    print("5")
